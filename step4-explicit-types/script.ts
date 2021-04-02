db8c05dd8c60bd526d8663f1f419d404b455c2e8
Muhammad Muneeb Waseem <50763652+MUHAMMAD-MUNEEB-WASEEM@users.noreply.github.com>
2021-04-02 16:33:56 +0500

    initial push of explicit types in typescript

---
+++ step4-explicit-types/script.ts
@@ -0,0 +1,23 @@
+//String
+
+let character:string;
+character = 'mario';
+
+//Error
+//character = 5 -> as string
+
+//Number
+
+let age: number;
+age = 2;
+
+//Error
+//age = true -> as number
+
+//String
+
+let isBlackBelt:boolean;
+isBlackBelt = true;
+
+//Error
+//isBlackBelt = 'hi -> as bool
