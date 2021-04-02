ceba775bb1613a34bc573900fe7cbf66927ad0d5
Muhammad Muneeb Waseem <50763652+MUHAMMAD-MUNEEB-WASEEM@users.noreply.github.com>
2021-04-02 12:24:33 +0500

    typescript basic types

---
+++ step2-type-basics/script.ts
@@ -0,0 +1,20 @@
+//STRING
+let character = 'mario';
+
+
+//character = 20 Gives Errir
+
+//NUMBER
+let age = 20
+
+//BOOLEAN
+let isBlackBelt = false;
+
+//FUNCTION
+//must define type in parameter
+
+const circ = (diameter:number) => {
+    return diameter * Math.PI
+}
+
+console.log(circ(1));
